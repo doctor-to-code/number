a5127ceb09bc6d013865cebbcc5ba533eacf5c31
Peter Yang <fgdeeerry@gmail.com>
2020-12-25 10:23:26 +0800

    second commit

--- r.py
+++ r.py
@@ -1,11 +1,13 @@
 import random
 
-r = random. randint(1, 5)
+r = random. randint(1, 100)
 while True:
 	x = input('請輸入數字')
 	x = int(x) 
 	if x == r:
 		print('you win')
 		break
-	else:
-		print('retry it')
+	elif x > r:
+		print('比答案大')
+	elif x < r:
+		print('比答案小')
